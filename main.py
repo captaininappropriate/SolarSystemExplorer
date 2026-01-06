bac288c635a2b43fcb833dd5511f936f1b6298d4
captaininappropriate <greg.nimmo@protonmail.com>
2026-01-06 20:02:55 +1100

    create required files and tested import

---
+++ main.py
@@ -0,0 +1,13 @@
+# Title: Solar System Explorer
+# Description: A Python program that allows users to explore information about the planets in our solar system.
+# Author: Greg Nimmo
+# Date: 06/01/2026
+# Version: 1.0
+# License: GNU General Public License v3.0
+# GitHub: https://github.com/captaininappropriate/SolarSystemExplorer
+
+from constants import API_KEY
+
+
+#print(f"Using API Key: {API_KEY}") # For demonstration purposes to show the API key is imported correctly.
+
